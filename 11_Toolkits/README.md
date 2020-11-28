3463e967dc1b5c493a10871681e01738c3004f53
KorotkovBS <only77nt@gmail.com>
2020-11-28 19:55:07 +0300

    readme

---
+++ 11_Toolkits/README.md
@@ -0,0 +1,8 @@
+## Toolkits - Glib
+Программа выводит количество вхождений слов в текст в порядке убывания.
+
+## Сборка
+1. git clean -fd && autoreconf -fisv &&./configure && make
+2. make test
+3. make run (если хочется ввести текст самостоятельно)
+4. make gitclean
--- 11_Toolkits/main.c
+++ 11_Toolkits/main.c
@@ -4,6 +4,19 @@
 
 #define BUFFER_SIZE 80
 
+gboolean finder(gpointer key, gpointer value, gpointer user_data) {
+	return !g_ascii_strcasecmp(key, user_data);
+}
+
+void print_map_iterator(gpointer key, gpointer value, gpointer user_data) {
+    printf("%s%s%d\n", (gchar*)key, (char*)user_data, *(gint*)value);
+}
+
+void print_list_iterator(gpointer key, gpointer map) {
+    gpointer value = g_hash_table_find(map, (GHRFunc)finder, key);
+    printf("%s:%d\n", (gchar*)key, *(gint*)value);
+}
+
 int main(int argc, char* argv[argc]) {
     FILE* input;
     switch (argc)
@@ -24,16 +37,57 @@ int main(int argc, char* argv[argc]) {
     }
 
     gchar buffer[BUFFER_SIZE+1];
-    gchar** words;
+
+    GSList* list = NULL, *node;
+
+    GHashTable* map = g_hash_table_new(g_str_hash, g_int_equal);
 
     while (fgets(buffer, BUFFER_SIZE+1, input)) {
         buffer[strlen(buffer) - 1] = '\0';
-		words = g_strsplit(buffer, " ", BUFFER_SIZE);
+		gchar** words = g_strsplit(buffer, " ", BUFFER_SIZE);
+
+        for (int i = 0; words[i]; i++) {
+            gpointer item_ptr = g_hash_table_find(map, (GHRFunc)finder, words[i]);
+            if (!item_ptr) {
+                gint* tmp = g_new(gint, 1);
+				*tmp = 1;
+                gchar* strtmp = g_strdup(words[i]);
+				g_hash_table_insert(map, strtmp, tmp);
+                g_hash_table_lookup(map, words[i]);
+				list = g_slist_append(list, strtmp);
+            } else {
+				(*(gint*)item_ptr)++;
+			}
+        }
     }
 
-    for (int i = 0; i < sizeof(words); i++) {
-        printf("%s\n", (char*)words[i]);
+    //g_hash_table_foreach(map, print_map_iterator, ":");
+
+    GSList* result = NULL;
+    int len = g_slist_length(list);
+
+    for(int i = 0; i < len; i++){
+        gint max = 0;
+        char* maxNode = NULL;
+        for(node = list; node; node = node->next){
+            //printf("%s ", (char*)node->data);
+            gpointer item_ptr = g_hash_table_find(map, (GHRFunc)finder, node->data);
+
+            if (max < *(gint*)item_ptr){
+                max = *(gint*)item_ptr;
+                maxNode = (char*)node->data;
+            }
+        }
+        //printf("\n");
+        result = g_slist_append(result, maxNode);
+        list = g_slist_remove(list, maxNode);
     }
 
+    g_slist_foreach(result, print_list_iterator, map);
+	
+	g_hash_table_destroy(map);
+	g_slist_free(list);
+    g_slist_free(result);
+   
     return 0;
 }
